2614240ecd86535c1d7593dc8be467f3caae5a5a
deepusiby100 <35959591+deepusiby100@users.noreply.github.com>
2022-10-06 14:13:28 +0200

    plugin added

---
+++ D365Plugin/AccountUpdate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace D365Plugin
+{
+    public class AccountUpdate : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
